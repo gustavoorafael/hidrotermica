b21e66ed8a7d83282935506cec3f7be86869ccf1
Gustavo Rafael <gustavoorafael270104@gmail.com>
2024-03-28 11:08:20 -0300

    alterando

--- src/main/java/org/example/calculo1.java
+++ src/main/java/org/example/calculo1.java
@@ -6,19 +6,18 @@ public class calculo1 {
         double[] pcv = {0.75215e3, 0.12284e-1, -0.12569e-5, 0.78525e-10, -0.19786e-14};
         double vmax = 12792.0;
         double vmin = 2412.0;
-        int n = 1;
 
-        double cotaMax = calcularCotaMax(pcv, vmax, n);
+        double cotaMax = calcularCotaMax(pcv, vmax);
         System.out.println("CotaMax: " + cotaMax);
 
-        double cotaMin = calcularCotaMin(pcv, vmin, n);
+        double cotaMin = calcularCotaMin(pcv, vmin);
         System.out.println("CotaMin: " + cotaMin);
 
         double resultadoFinal = calculaResultadoFinal(cotaMax, cotaMin, vmax, vmin);
         System.out.println("Resultado Final: " + resultadoFinal);
     }
 
-    public static double calcularCotaMax(double[] pcj, double vmax, int n) {
+    public static double calcularCotaMax(double[] pcj, double vmax) {
         double resultadoMax = 0.0;
         for (int i = 0; i < pcj.length; i++) {
             resultadoMax = resultadoMax + pcj[i] * Math.pow(vmax, i + 1) / (i + 1);
@@ -26,7 +25,7 @@ public class calculo1 {
         return resultadoMax;
     }
 
-    public static double calcularCotaMin(double[] pcj, double vmin, int n) {
+    public static double calcularCotaMin(double[] pcj, double vmin) {
         double resultadoMin = 0.0;
         for (int i = 0; i < pcj.length; i++) {
             resultadoMin = resultadoMin + pcj[i] * Math.pow(vmin, i + 1) / (i + 1);
